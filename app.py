ad125cac3af2f3a2d3dc904f98e9736513b0b304
Adrian <adrianestevezg@gmail.com>
2023-03-08 23:23:06 +0100

    simplest authentication

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import session
+from flask import request
+from flask import redirect, url_for
+
 
 from src.routes import user_bp
 
@@ -10,6 +14,33 @@ app.register_blueprint(user_bp)
 @app.route('/')
 def hello_world():
     return 'Web Scrapping'
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/profile')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
