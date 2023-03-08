6229cdf195bf10dceb44d6642e0917ca35c16ca7
Adrian <adrianestevezg@gmail.com>
2023-03-08 20:18:16 +0100

    get old repository files

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.controllers.user import UserController
+
+user_bp = Blueprint('user', __name__)
+user_controller = UserController()
+
+@user_bp.route('/users')
+def get_users():
+    return user_controller.get_users()
