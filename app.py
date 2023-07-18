f787123ebdf96db0811d926ae383f1379f3c5a79
mishelltigseortiz <mishelltigseortiz@gmail.com>
2023-07-18 19:31:52 +0200

    Spaces added

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 
 app.register_blueprint(user_bp)
 
+
 @app.route('/')
 def hello_world():
     return 'Web Scrapping'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
