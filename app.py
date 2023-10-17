3e5149cf57b81e5a34d4525b4ec7834484da060a
Mishell JaninaTigse <mishelljanina.tigse.next@bbva.com>
2023-10-17 09:34:24 +0200

    Tests with library scholarly

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import flask
 import src
 from src.bind import Bind
 from src.domain.base import Error
+from scholarly import scholarly
 
 app = None
 bind = None
@@ -27,6 +28,27 @@ def create_app():
 
         @app.route('/')
         def hello():
+            print("Búsqueda de autor por organización: (Juan Tamargo)")
+            search_query = scholarly.search_author_by_organization(3310657935946510136)
+            author = next(search_query)
+            scholarly.pprint(scholarly.fill(author, sections=['basics']))
+            print("Búsqueda de autor por su id: (Juan Antonio Clemente)")
+            search_query = scholarly.search_author_id('71ZyPn0AAAAJ')
+            scholarly.pprint(scholarly.fill(search_query, sections=['basics']))
+            print("Búsqueda por autor: (Juan Antonio Clemente)") # No lo reconoce bien
+            search_query = scholarly.search_author('Juan Antonio Clemente')
+            author = next(search_query)
+            scholarly.pprint(scholarly.fill(author, sections=['basics']))
+            print("Búsqueda por autor: (Juan Antonio Clemente, Universidad Complutense de Madrid)")  # Lo reconoce bien
+            search_query = scholarly.search_author('Juan Antonio Clemente, Universidad Complutense de Madrid')
+            author = next(search_query)
+            scholarly.pprint(scholarly.fill(author, sections=['basics', 'counts', 'coauthors', 'publications']))
+            print("Búsqueda por organización: ")
+            search_query = scholarly.search_org('Princeton University')
+            print(search_query)
+            print("Búsqueda por artículos/publicaciones: ")
+            search_query = scholarly.search_pubs('Cancer')
+            scholarly.pprint(next(search_query))
             return 'Welcome to Web Scrapping!'
 
 
